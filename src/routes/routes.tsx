2a48c832619bdca099645ec7e3a0b35250bd97f6
pockypoem <jeremyas.cornelis@gmail.com>
2024-01-08 20:59:52 +0700

    feat: add header and footer popup detail ticket

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
+import DummyDetailTiket from "../pages/ListTiket/dummy";
 
 function Router() {
     return (
