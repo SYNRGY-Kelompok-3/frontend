18c45d681bcdae61afda0d70da93c8e70cd5f7b8
“ARRARIAKU2” <yerry_kecak@yahoo.com>
2024-01-09 18:42:51 +0700

    Navbar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "../pages/Home";
 
 function Router() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+
                 </Routes>
             </BrowserRouter>
         </>
