5baeb6fee8b462990f2d8c04541ebb8ef2cad0e9
“ARRARIAKU2” <yerry_kecak@yahoo.com>
2024-01-09 17:40:18 +0700

    Add Footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,8 @@
-import Navbar from "../../components/organisms/Navbar";
-import Filter from "../../components/organisms/FilterHome";
 import Footer from "../../components/organisms/Footer";
 
 function Home() {
     return (
-        <>
-            <Navbar bg={"bg-transparent"} />
-            <div className="flex justify-center align-center">
-                <h2 className="w-[1100px] text-center font-semibold text-6xl mt-[50px] leading-[normal]">Cari Tiket Murah dan Cepat Bersama Kami</h2>
-            </div>
-            <Filter />
-            <Footer />
-        </>
+        <Footer />
     );
 };
 
