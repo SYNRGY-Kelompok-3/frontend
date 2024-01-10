334596223307777a025c0303a70cb0b45e22ff69
angga-thetreedots <angga.saputra@thetreedots.com>
2024-01-10 22:59:42 +0700

    fix: edit steps staging env

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,15 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DummyDetailTiket from "../pages/ListTiket/dummy";
 
 function Router() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/detailTiket" element={<DummyDetailTiket />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
-};
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/detailTiket" element={<DummyDetailTiket />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
 export default Router;
