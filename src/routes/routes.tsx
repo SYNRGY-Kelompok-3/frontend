81e74f6cda52048492bc441f4ca5c05ee0552eb5
“ARRARIAKU2” <yerry_kecak@yahoo.com>
2024-01-10 16:53:43 +0700

    -

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
-import Tentang from "../pages/Tentang";
-import Artikel from "../pages/Artikel";
-import Pusat from "../pages/Pusat";
 
 function Router() {
     return (
@@ -11,9 +8,6 @@ function Router() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/tentang-kami" element={<Tentang />} />
-                    <Route path="/artikel" element={<Artikel />} />
-                    <Route path="/pusat-bantuan" element={<Pusat />} />
                 </Routes>
             </BrowserRouter>
         </>
