7b9a95c381cc44c713e4a92d18e6c00b38714439
“ARRARIAKU2” <yerry_kecak@yahoo.com>
2024-01-11 22:24:06 +0700

    fix: semantic tag

--- src/layout/Page.tsx
+++ src/layout/Page.tsx
@@ -3,13 +3,13 @@ import Navbar from "src/components/organisms/Navbar";
 import Footer from "src/components/organisms/Footer";
 
 function Layout() {
-    return (
-        <div>
-            <Navbar bg={"bg-white shadow-md"} />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg={"bg-white shadow-md"} />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
