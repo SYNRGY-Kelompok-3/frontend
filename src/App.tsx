6facd91cda6a5274141ca83908a6329be0da6825
angga-22 <anggasp.unib@gmail.com>
2024-01-12 00:18:21 +0700

    fix: test branching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Router from './routes/routes'
+import Router from "./routes/routes";
 
 function App() {
+  console.log(`tes `);
   return (
     <>
       <Router />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
