3a299a18139c19f31e5bad94ed4f1590dba028d1
“ARRARIAKU2” <yerry_kecak@yahoo.com>
2024-01-11 22:23:49 +0700

    fix: semantic tag

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
 import Filter from "src/components/organisms/FilterHome";
 
 function Home() {
-    return (
-        <>
-            <body>
-                <div className="flex justify-center align-center">
-                    <h2 className="w-[1100px] text-white text-center font-semibold text-6xl mt-[50px] leading-[normal]">
-                        Cari Tiket Murah dan Cepat Bersama Kami
-                    </h2>
-                </div>
-                <Filter />
-            </body>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <div className="flex justify-center align-center">
+          <h2 className="w-[1100px] text-white text-center font-semibold text-6xl mt-[50px] leading-[normal]">
+            Cari Tiket Murah dan Cepat Bersama Kami
+          </h2>
+        </div>
+        <Filter />
+      </main>
+    </>
+  );
 }
 
 export default Home;
