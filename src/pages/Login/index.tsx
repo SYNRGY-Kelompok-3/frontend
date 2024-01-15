89ec5ef29cfcfe8e90fc9c46d69cc367717a69b4
“ARRARIAKU2” <yerry_kecak@yahoo.com>
2024-01-15 12:03:59 +0700

    fix: login page view

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,9 +7,9 @@ function Login() {
   return (
     <main>
       <div className="flex flex-row">
-        <div className="basis-1/2 m-4">
-          <div className="h-[100%] bg-[url('src/assets/bg.jpg')] rounded-xl">
-            <div className="h-full bg-violet-500 bg-opacity-60 rounded-xl">
+        <div className="basis-1/2">
+          <div className="h-screen bg-[url('src/assets/bglogin.png')] bg-cover">
+            <div className="h-full bg-violet-500 bg-opacity-20">
               <div className="py-[160px]">
                 <Link to="/" className="flex justify-center">
                   <Image src={Logo} alt={"logo"} className={"w-[25px] h-[25px] mr-2"} />
@@ -27,55 +27,63 @@ function Login() {
           </div>
         </div>
         <div className="basis-1/2 m-4">
-          <div className="my-[200px] flex justify-center">
-            <div className="w-[70%] flex flex-col min-w-0 break-words bg-transparent border-0 lg:py4 dark:bg-gray-950 rounded-2xl bg-clip-border shadow">
+          <div className="my-[200px] flex">
+            <div className="w-[100%] flex flex-col min-w-0 break-words bg-transparent border-0 lg:py4 dark:bg-gray-950 rounded-2xl bg-clip-border shadow">
               <div className="p-6 pb-0 mb-0">
-                <div className="font-bold text-xl">Sign In</div>
-                <div className="pt-3">Enter your email and password to sign in</div>
+                <div className="font-bold text-3xl">Masuk ke Akunmu</div>
+                <div className="py-3">
+                  <p className="leading-normal">
+                    Nikmati perjalananmu bersama kami. Belum punya akun?
+                    <Link
+                      to="/register"
+                      className="font-semibold text-transparent bg-clip-text bg-gradient-to-tl from-blue-500 to-violet-500 mx-1"
+                    >
+                      Daftar
+                    </Link>
+                  </p>
+                </div>
               </div>
-              <div className="p-6">
+              <div className="px-6">
                 <form role="form">
-                  <div className="mb-4">
+                  <div className="">
+                    <div className="py-2">Email</div>
                     <input
                       type="email"
                       placeholder="Email"
                       className="focus:shadow-primary-outline dark:bg-gray-950 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding p-3 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none"
                     />
                   </div>
-                  <div className="mb-4">
+                  <div className="">
+                    <div className="py-2">Password</div>
                     <input
                       type="password"
                       placeholder="Password"
                       className="focus:shadow-primary-outline dark:bg-gray-950 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding p-3 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none"
                     />
                   </div>
-                  <div className="flex items-center mb-0.5 text-left min-h-6">
-                    <input id="rememberMe" className="w-4 h-4 mt-0.5 rounded" type="checkbox" />
-                    <label className="ml-2 font-normal cursor-pointer select-none text-sm text-slate-700">
-                      Remember me
-                    </label>
+                  <div className="pt-5 flex justify-between items-center text-left min-h-6 ">
+                    <div className="flex items-center">
+                      <input id="rememberMe" className="w-5 h-5 mt-0.5 rounded" type="checkbox" />
+                      <label className="ml-2 font-normal cursor-pointer select-none text-slate-700">
+                        Remember me
+                      </label>
+                    </div>
+                    <Link
+                      to="/lupa-password"
+                      className="font-semibold text-transparent bg-clip-text bg-gradient-to-tl from-blue-500 to-violet-500"
+                    >
+                      Lupa Password?
+                    </Link>
                   </div>
-                  <div className="text-center">
+                  <div className="text-center my-6">
                     <button
                       type="button"
-                      className="inline-block w-full px-16 py-3.5 mt-6 mb-0 font-bold leading-normal text-center text-white align-middle transition-all bg-blue-500 border-0 rounded-lg cursor-pointer hover:-translate-y-px active:opacity-85 hover:shadow-xs text-sm ease-in tracking-tight-rem shadow-md bg-150 bg-x-25"
+                      className="inline-block w-full px-16 py-3.5 mb-0 font-bold leading-normal text-center text-white align-middle transition-all bg-blue-500 border-0 rounded-lg cursor-pointer hover:-translate-y-px active:opacity-85 hover:shadow-xs text-sm ease-in tracking-tight-rem shadow-md bg-150 bg-x-25"
                     >
-                      Sign in
+                      Masuk
                     </button>
                   </div>
                 </form>
-                <div className="text-center pt-6">
-                  <p className="leading-normal text-sm">
-                    Don't have an account?
-                    <Link
-                      to="/register"
-                      className="font-semibold text-transparent bg-clip-text bg-gradient-to-tl from-blue-500 to-violet-500"
-                    >
-                      {" "}
-                      Sign up
-                    </Link>
-                  </p>
-                </div>
               </div>
             </div>
           </div>
